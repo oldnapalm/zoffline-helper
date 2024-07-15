913b2f3762651fe83baaa3ab86b8f7cf7c678a3c
oldnapalm <38410858+oldnapalm@users.noreply.github.com>
2024-07-15 20:55:15 -0300

    Download economy config

--- protobuf/make.bat
+++ protobuf/make.bat
@@ -1,5 +1,6 @@
 del *_pb2.py *_pb2.pyc
 protoc --python_out=. activity.proto
+protoc --python_out=. login.proto
 protoc --python_out=. profile.proto
 protoc --python_out=. per-session-info.proto
 protoc --python_out=. udp-node-msgs.proto
